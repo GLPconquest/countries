9d87940b40d3821f31209c9b5c72ac407cc2b4e4
Alexis Constant <35760095+AlexisConstant@users.noreply.github.com>
2018-01-31 10:02:39 +0100

    Add files via upload

---
+++ Ability.java
@@ -0,0 +1,21 @@
+package countries;
+
+public class Ability {
+	
+	int boostRange;
+	int boostMovement;
+	int boostAttack;
+	int boostDefense;
+	Resources boostCost;
+	Resources boostResources;
+	Resources boostFood;
+	Resources boostOil;
+	Resources boostEletricity;
+	public Ability(int boostRange, int boostMovement, int boostAttack, int boostDefense, Resources boostCost,
+			Resources boostResources, Resources boostFood, Resources boostOil, Resources boostEletricity) {
+		
+		
+		
+	}
+
+}
